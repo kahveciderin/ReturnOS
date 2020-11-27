f22403118d35e1d5a480d238cad68ae0163433b6
kahveciderin <kahveciderin@gmail.com>
2020-11-27 17:28:41 +0300

    \n

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -62,7 +62,7 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_RED, VGA_COLOR_BLACK);
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -85,18 +85,32 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
  
 void terminal_putchar(char c) 
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
+
+	switch(c){
+		case '\n':
+			if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
+		terminal_column = 0;
+		break;
+
+		default:
+			terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+			if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+			}
+		break;
 	}
+
 }
  
 void terminal_write(const char* data, size_t size) 
 {
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++){
 		terminal_putchar(data[i]);
+	}
+
 }
  
 void terminal_writestring(const char* data) 
@@ -110,5 +124,5 @@ void kernel_main(void)
 	terminal_initialize();
  
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("haha no");
 }
